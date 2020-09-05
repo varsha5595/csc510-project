08aeaddd40daa4e087efebe1660fb54c714277b5
shiva96b <shiva96b@gmail.com>
2020-09-05 19:32:12 -0400

    Slack Integration

---
+++ server.py
@@ -0,0 +1,44 @@
+# import the random library to help us generate the random numbers
+import random
+
+# Create the CoinBot Class
+class CoinBot:
+
+    # Create a constant that contains the default text for the message
+    COIN_BLOCK = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                "Sure! Flipping a coin....\n\n"
+            ),
+        },
+    }
+
+    # The constructor for the class. It takes the channel name as the a
+    # parameter and then sets it as an instance variable
+    def __init__(self, channel):
+        self.channel = channel
+
+    # Generate a random number to simulate flipping a coin. Then return the
+    # crafted slack payload with the coin flip message.
+    def _flip_coin(self):
+        rand_int =  random.randint(0,1)
+        if rand_int == 0:
+            results = "Heads"
+        else:
+            results = "Tails"
+
+        text = "The result is {results}"
+
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}},
+
+    # Craft and return the entire message payload as a dictionary.
+    def get_message_payload(self):
+        return {
+            "channel": self.channel,
+            "blocks": [
+                self.COIN_BLOCK,
+                self._flip_coin(),
+            ],
+        }
---
+++ server_test.py
@@ -0,0 +1,14 @@
+from slack import WebClient
+from coinbot import CoinBot
+import os
+
+# Create a slack client
+slack_web_client = WebClient(token = "")
+# Get a new CoinBot
+coin_bot = CoinBot("#postman")
+
+# Get the onboarding message payload
+message = coin_bot.get_message_payload()
+
+# Post the onboarding message in Slack
+slack_web_client.chat_postMessage(**message)
