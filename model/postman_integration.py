8692fa654151b0ca7e58e9efb832a632663d1cac
Adithya Raghu Ganesh <adithyarganesh@gmail.com>
2020-09-07 23:18:13 -0400

    Add regex for fetching changes

--- model/postman_integration.py
+++ model/postman_integration.py
@@ -3,6 +3,7 @@ from jsondiff import diff
 import json
 import os
 import mimetypes
+import re
 
 
 def get_postman_collections(connection):
@@ -15,6 +16,10 @@ def get_postman_collections(connection):
     connection.request("GET", "/collections", payload, headers)
     res = connection.getresponse()
     data = res.read()
+
+def regex(value):
+    regexes = ["(?<='key': )[^,||}]*", "(?<='value': )[^,||}]*", "(?<=delete: \[)[^]]+", "(?<=insert: \[)[^]]+"]
+    return [re.findall(regex,value) for regex in regexes] 
 
 
 def get_selected_collection(collection_id, connection):
@@ -30,17 +35,21 @@ def get_selected_collection(collection_id, connection):
 
     with open("../data/collections/" + collection_id + ".txt", "r+") as f:
         old_value = diff(data, json.load(f))
+        body_old = re.findall("(?<='body': )[^,||}]*", re.sub('\s+', '', old_value))
         print(old_value)
         f.close()
 
     with open("../data/collections/" + collection_id + ".txt", "r+") as f:
         new_value = diff(json.load(f), data)
+        body_new = re.sub('\s+', '', new_value)
         print(new_value)
         f.close()
 
     with open("../data/collections/" + collection_id + ".txt", "w+") as f:
         json.dump(data, f)
         f.close()
+
+    print([regex(value) for value in [diff(body_old,body_new), diff(body_new,body_old), old_value, new_value]])
 
 
 def main():
