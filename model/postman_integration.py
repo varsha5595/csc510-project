58d0ad166c8597f6918a0cd353e69282fa2fa8ce
Varsha Sharma <varsha95.ananth@gmail.com>
2020-09-20 19:18:14 -0400

    Adding command line user interaction

--- model/postman_integration.py
+++ model/postman_integration.py
@@ -1,4 +1,5 @@
 import http.client
+import jsondiff
 from jsondiff import diff
 import json
 import os
@@ -8,84 +9,120 @@ import time
 import slack
 from slack import WebClient
 import ssl
+import sys
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def get_postman_collections(connection):
+def get_postman_collections(connection, api_key):
     boundary = ''
     payload = ''
     headers = {
-        'X-Api-Key': '',
+        'X-Api-Key': api_key,
         'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
     }
     connection.request("GET", "/collections", payload, headers)
-    res = connection.getresponse()
-    data = res.read()
+    response = connection.getresponse()
+    if response.status == 200:
+        return response
+    else:
+        raise Exception("Exited with status code " + str(response.status) + '. '+ str(json.loads(response.read())['error']['message']))
 
-    
+
 def regex(value):
     regexes = ["(?<='key': )[^,||}]*", "(?<='value': )[^,||}]*", "(?<=delete: \[)[^]]+", "(?<=insert: \[)[^]]+"]
     return [re.findall(regex, value) for regex in regexes]
 
 
-def get_selected_collection(collection_id, connection):
+def get_selected_collection(collection_id, connection, api_key):
     boundary = ''
     payload = ''
     headers = {
-        'X-Api-Key': '',
+        'X-Api-Key': api_key,
         'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
     }
     connection.request("GET", "/collections/" + collection_id, payload, headers)
-    res = connection.getresponse()
-    data = json.loads(res.read())
+    response = connection.getresponse()
+    if response.status == 200:
+        data = json.loads(response.read())
 
-    with open("../data/collections/" + collection_id + ".txt", "r+") as f:
-        old_value = diff(data, json.load(f))
-        print(old_value)
-        f.close()
+        filepath = "./data/" + collection_id + ".txt"
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        if not os.path.exists(filepath):
+            with open(filepath, "w") as f:
+                f.write("{}")
+                f.close()
+        
+        with open(filepath, "r+") as f:
+            old_value = diff(data, json.load(f))
+            f.close()
 
-    with open("../data/collections/" + collection_id + ".txt", "r+") as f:
-        new_value = diff(json.load(f), data)
-        print(new_value)
-        f.close()
+        with open(filepath, "r+") as f:
+            new_value = diff(json.load(f), data)
+            f.close()
 
-    with open("../data/collections/" + collection_id + ".txt", "w+") as f:
-        json.dump(data, f)
-        f.close()
+        changes = [regex(str(value)) for value in [old_value, new_value]]
 
-    changes = [regex(str(value)) for value in [old_value, new_value]]
-    keys_old = "Keys in old call: " + ' '.join(changes[0][0])
-    keys_new = "Keys in new call: " + ' '.join(changes[1][0])
-    ins = "pairs inserted in new call: " + ' '.join(changes[1][3])
-    dels = "Row deleted from old call: " + ' '.join(changes[0][2])
-    
-    return keys_old + "\n" + keys_new + "\n" + ins + "\n" + dels
+        if changes:
+            with open(filepath, "w+") as f:
+                json.dump(data, f)
+                f.close()
+        
+        keys_old = "Keys in old call: " + ' '.join(changes[0][0])
+        keys_new = "Keys in new call: " + ' '.join(changes[1][0])
+        ins = "Pairs inserted in new call: " + ' '.join(changes[1][3])
+        dels = "Row deleted from old call: " + ' '.join(changes[0][2])
+        
+        return keys_old + "\n" + keys_new + "\n" + ins + "\n" + dels
+    else:
+        raise Exception("Exited with status code " + str(response.status) + '. '+ str(json.loads(response.read())['error']['message']))
 
 
 def main():
-    postman_connection = http.client.HTTPSConnection("api.getpostman.com")
-    get_postman_collections(postman_connection)
-    colls = get_selected_collection("", postman_connection)
-    
-    # Create a slack client
-    slack_web_client = WebClient(token = "")
+    try:
+        postman_connection = http.client.HTTPSConnection("api.getpostman.com")
 
-    # Slack Channel to post the message
-    channel = "postman"
+        print("\nWelcome to Sync Ends! End your development overheads today!")
+        print("-----------------------------------------------------------")
+        print("\nLet's begin by linking your Postman account with our service!")
+        api_key = input("Enter the API key for you Postman account: ")
 
-    # Get the onboarding message payload
-    message = {
-                "channel": channel,
-                "blocks": [
-                    { "type": "section", "text": { "type": "plain_text", "text": colls}}
-                ],
-            }
+        collections_response = get_postman_collections(postman_connection, api_key)
+        all_collections = json.loads(collections_response.read())
+        
+        print("\nChoose a collection you would like to integrate:")
 
-    # Post the onboarding message in Slack
-    slack_web_client.chat_postMessage(**message)
+        for index, collection in enumerate(all_collections['collections'], 1):
+            print(str(index) + '. ' + str(collection['name'])) 
+
+        collection_choice = input("\nEnter your choice: ")
+        selected_collection = all_collections['collections'][int(collection_choice) - 1]
+
+        print("\nAlmost there, let's link the Slack bot to our service!")
+        slack_token = input("Enter the API token for the Slack bot: ")
+        
+        while True:
+            changes_detected = get_selected_collection(selected_collection['uid'], postman_connection, api_key)
+            
+            # Create a slack client
+            slack_web_client = WebClient(token = slack_token)
+
+            # Slack Channel to post the message
+            channel = "postman"
+
+            # Get the onboarding message payload
+            message = {
+                        "channel": channel,
+                        "blocks": [
+                            { "type": "section", "text": { "type": "plain_text", "text": changes_detected}}
+                        ],
+                    }
+
+            # Post the onboarding message in Slack
+            slack_web_client.chat_postMessage(**message)
+            time.sleep(3600)
+    except Exception as e:
+        print(e)
 
     
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(3600)
+    main()
