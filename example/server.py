2d2aea336b032a077b4223d46d2cd65c22b9b30c
shiva96b <shiva96b@gmail.com>
2020-09-20 22:09:29 -0400

    Updating to Parameterized Queries and Adding Tests

--- example/server.py
+++ example/server.py
@@ -4,26 +4,32 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask import jsonify
 
+# Connecting to the DB
 db_connect = create_engine('sqlite:///sample.db')
+
+# Creating the application
 app = Flask(__name__)
 api = Api(app)
 
-class Employees(Resource):
+# API to get the list of all the empoyees
+class Employee(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from employees") # This line performs query and returns json result
         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is employee ID
 
-class Employees_Name(Resource):
-    def get(self, employee_id):
+# API to get info about an empoyee
+class Employee_info(Resource):
+    def get(self):
         conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+        emp = request.args.get('employee_id')
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(emp))
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
-        
 
-api.add_resource(Employees, '/employees') # Route 1
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route 2
+# Publishing APIs
+api.add_resource(Employee, '/employees') # Route 1
+api.add_resource(Employee_info, '/employee') # Route 2
 
 if __name__ == '__main__':
     app.run(port=5002)
