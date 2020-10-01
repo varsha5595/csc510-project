6cf91e07388f0f2cba152bf6c0a558950b57cce6
oaaky <omkar.omkar.135@gmail.com>
2020-10-01 02:57:12 -0400

    parsing the user input and collecting in variables

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from src.parser import Parser
 def main():
     # get the arguments from commadn line
     parser = Parser()
+    collection_name, api_key, trigger_interval, slack_channel = parser.get_argumenets()
 
 
 if __name__ == "__main__":
