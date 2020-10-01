c5b8281a30e731dc09d05c584c43ec134170f9a8
oaaky <omkar.omkar.135@gmail.com>
2020-10-01 02:56:25 -0400

    adding the driver class to call invoke the sync end class methods

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("..")
+
+from src.sync_ends_service import SyncEnd
+from src.parser import Parser
+
+
+def main():
+    # get the arguments from commadn line
+    parser = Parser()
+
+
+if __name__ == "__main__":
+    main()
