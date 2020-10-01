a68e0599af6d047af207d20cb74a1c61a7722c50
oaaky <omkar.omkar.135@gmail.com>
2020-10-01 02:54:52 -0400

    adding the getter methods to the class

--- src/end_point.py
+++ src/end_point.py
@@ -10,3 +10,24 @@ class EndPoint:
         self.header = end_point_json['request'].get('header')
         self.url = end_point_json['request']['url']['raw']
         self.query_parameters = end_point_json['request']['url'].get('query')
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_authentication(self):
+        return self.authentication
+
+    def get_method(self):
+        return self.method
+
+    def get_header(self):
+        return self.header
+
+    def get_url(self):
+        return self.url
+
+    def get_query_parameters(self):
+        return self.query_parameters
