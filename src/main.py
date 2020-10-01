6b414132c7089dc0ab28e4655d9cd5eaa803d39a
oaaky <omkar.omkar.135@gmail.com>
2020-10-01 02:57:54 -0400

    creating sync end class object with user CLI parameters

--- src/main.py
+++ src/main.py
@@ -9,6 +9,11 @@ def main():
     # get the arguments from commadn line
     parser = Parser()
     collection_name, api_key, trigger_interval, slack_channel = parser.get_argumenets()
+    sync_end = SyncEnd(api_key, collection_name, trigger_interval, slack_channel)
+    try:
+        sync_end.start()
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
