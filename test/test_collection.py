994f61ae3d2f28837376f65e889c66f47f942a51
cagandhi <cagandhi97@gmail.com>
2020-10-25 16:27:42 -0400

    add setup method to remove redundant collection creation

--- test/test_collection.py
+++ test/test_collection.py
@@ -1,10 +1,53 @@
 import unittest
 from unittest.mock import Mock
-
 from src.collection import Collection
 
 
 class TestCollection(unittest.TestCase):
+    def setUp(self):
+        json_dict = {
+            "info": {
+                "_postman_id": "1757f50c-60d1-4a48-a552-baa96d722e0f",
+                "name": "Sample Server",
+                "description": "This collection contains sample APIs that can be used to test the Sync Ends service.",  # noqa: E501
+                "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",  # noqa: E501
+            },
+            "item": [
+                {
+                    "name": "Get employee details",
+                    "_postman_id": "385f7848-62db-4435-b7cf-820c3e7e5097",
+                    "protocolProfileBehavior": {"disableBodyPruning": True},
+                    "request": {
+                        "method": "POST",
+                        "header": [],
+                        "url": {
+                            "raw": "http://127.0.0.1:5002/employee?emp_id=1",
+                            "protocol": "http",
+                            "host": ["127", "0", "0", "1"],
+                            "port": "5002",
+                            "path": ["employee"],
+                            "query": [{"key": "emp_id", "value": "1"}],
+                        },
+                    },
+                    "response": [],
+                }
+            ],
+        }
+
+        self.collection = Collection(json_dict)
+
+        self.endpoint = Mock()
+        self.endpoint.id = "385f7848-62db-4435-b7cf-820c3e7e5097"
+        self.endpoint.name = "Get employee details"
+        self.endpoint.authentication = None
+        self.endpoint.method = "POST"
+        self.endpoint.header = []
+        self.endpoint.url = "http://127.0.0.1:5002/employee?emp_id=1"
+        self.endpoint.query_parameters = [{"key": "emp_id", "value": "1"}]
+
+    def tearDown(self):
+        pass
+
     def test_get_end_points(self):
         def compareObj(obj1, obj2):
             if (
@@ -19,85 +62,14 @@ class TestCollection(unittest.TestCase):
                 return True
             return False
 
-        json_dict = {
-            "info": {
-                "_postman_id": "1757f50c-60d1-4a48-a552-baa96d722e0f",
-                "name": "Sample Server",
-                "description": "This collection contains sample APIs that can be used to test the Sync Ends service.",  # noqa: E501
-                "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",  # noqa: E501
-            },
-            "item": [
-                {
-                    "name": "Get employee details",
-                    "_postman_id": "385f7848-62db-4435-b7cf-820c3e7e5097",
-                    "protocolProfileBehavior": {"disableBodyPruning": True},
-                    "request": {
-                        "method": "POST",
-                        "header": [],
-                        "url": {
-                            "raw": "http://127.0.0.1:5002/employee?emp_id=1",
-                            "protocol": "http",
-                            "host": ["127", "0", "0", "1"],
-                            "port": "5002",
-                            "path": ["employee"],
-                            "query": [{"key": "emp_id", "value": "1"}],
-                        },
-                    },
-                    "response": [],
-                }
-            ],
-        }
-
-        collection = Collection(json_dict)
-
-        endpoint = Mock()
-        endpoint.id = "385f7848-62db-4435-b7cf-820c3e7e5097"
-        endpoint.name = "Get employee details"
-        endpoint.authentication = None
-        endpoint.method = "POST"
-        endpoint.header = []
-        endpoint.url = "http://127.0.0.1:5002/employee?emp_id=1"
-        endpoint.query_parameters = [{"key": "emp_id", "value": "1"}]
-
-        input_end_points = collection.get_end_points()[0]
-        self.assertTrue(compareObj(endpoint, input_end_points))
+        input_end_points = self.collection.get_end_points()[0]
+        self.assertTrue(compareObj(self.endpoint, input_end_points))
 
     def test_remove_end_point(self):
 
-        json_dict = {
-            "info": {
-                "_postman_id": "1757f50c-60d1-4a48-a552-baa96d722e0f",
-                "name": "Sample Server",
-                "description": "This collection contains sample APIs that can be used to test the Sync Ends service.",  # noqa: E501
-                "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",  # noqa: E501
-            },
-            "item": [
-                {
-                    "name": "Get employee details",
-                    "_postman_id": "385f7848-62db-4435-b7cf-820c3e7e5097",
-                    "protocolProfileBehavior": {"disableBodyPruning": True},
-                    "request": {
-                        "method": "POST",
-                        "header": [],
-                        "url": {
-                            "raw": "http://127.0.0.1:5002/employee?emp_id=1",
-                            "protocol": "http",
-                            "host": ["127", "0", "0", "1"],
-                            "port": "5002",
-                            "path": ["employee"],
-                            "query": [{"key": "emp_id", "value": "1"}],
-                        },
-                    },
-                    "response": [],
-                }
-            ],
-        }
-
-        collection = Collection(json_dict)
-
-        end = collection.get_end_points()[0]
-        collection.remove_end_point(end)
-        self.assertEqual(len(collection.end_points), 0)
+        endpoint = self.collection.get_end_points()[0]
+        self.collection.remove_end_point(endpoint)
+        self.assertEqual(len(self.collection.end_points), 0)
 
 
 if __name__ == "__main__":
