c27e079474bd887857b367061725f0aa23736655
jaymodi98 <jaymodi98@gmail.com>
2020-10-25 18:40:14 -0400

    removed formatting error

--- src/main.py
+++ src/main.py
@@ -2,15 +2,22 @@ import sys
 
 sys.path.append("..")
 
-from src.sync_ends_service import SyncEnd
-from src.parser import Parser
+from src.sync_ends_service import SyncEnd  # noqa: 402
+from src.parser import Parser  # noqa: 402
 
 
 def main():
     # get the arguments from commadn line
     parser = Parser()
-    collection_name, api_key, trigger_interval, slack_channel = parser.get_argumenets()
-    sync_end = SyncEnd(api_key, collection_name, trigger_interval, slack_channel)
+    (
+        collection_name,
+        api_key,
+        trigger_interval,
+        slack_channel,
+    ) = parser.get_argumenets()
+    sync_end = SyncEnd(
+        api_key, collection_name, trigger_interval, slack_channel
+    )
     try:
         sync_end.start()
     except Exception as e:
