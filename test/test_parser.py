46371621689583c33bdb8627a4256a80f3308b61
sj23patel <suraj23patel@gmail.com>
2020-10-26 14:28:56 -0400

    Update parser test with each assert statements

--- test/test_parser.py
+++ test/test_parser.py
@@ -14,7 +14,19 @@ class TestParser(unittest.TestCase):
         
     @mock.patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(collection_name='test server', key='SAM-Key-123fg', trigger_interval=9, slack_channel='sample channel', slack_token='123ff'))
     def test_get_args(self,mock_args):
-        pass
+        (
+        collection_name,
+        api_key,
+        trigger_interval,
+        slack_channel,
+        slack_token,
+        ) = self.parser.get_arguments()
+        # print(args)
+        self.assertEqual(collection_name, 'test server')
+        self.assertEqual(api_key, 'SAM-Key-123fg')
+        self.assertEqual(trigger_interval, 9)
+        self.assertEqual(slack_channel, 'sample channel')
+        self.assertEqual(slack_token, '123ff')
     
 if __name__ == "__main__":
     unittest.main()
