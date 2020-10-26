aeb6d9a002cae889f120b124c2f076fa0cb0d6ac
jaymodi98 <jaymodi98@gmail.com>
2020-10-26 05:59:28 -0400

    checking if single line command works when only flake8 is failing

--- src/collection.py
+++ src/collection.py
@@ -3,7 +3,7 @@ from src.end_point import EndPoint
 
 class Collection:
     def __init__(self, collection_json):
-        self.end_points = [EndPoint(x) for x in collection_json["item"]]
+        self.end_points=[EndPoint(x) for x in collection_json["item"]]
 
     def get_end_points(self):
         return self.end_points
