a6355211cc6782452cd7ac398343a351369f2a0f
Nirav <shahniravalpesh@gmail.com>
2020-10-26 17:13:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@ def get_long_description(filename):
 
 setuptools.setup(
     name="sync-ends",
-    version="1.0.0",
+    version="v2.0.1",
+=======
+    version="v2.0.0",
+>>>>>>> 936ce9b11ae7517b47ac6d9d803aeaaadccd2d3e
     author="Chintan Gandhi, Jay Modi, Suraj Patel, Omkar Kulkarni, Nirav Shah",
     author_email="cagandhi97@gmail.com",
     description="Sync Ends - End Development Overheads CSC 510 Project",
@@ -27,6 +30,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     keywords=["python"],
     url="https://github.com/jaymodi98/csc510-project",
+    download_url = 'https://github.com/jaymodi98/Sync-Ends/archive/v2.0.1.tar.gz',
+=======
+    download_url = 'https://github.com/jaymodi98/Sync-Ends/archive/v2.0.0.tar.gz',
+>>>>>>> 936ce9b11ae7517b47ac6d9d803aeaaadccd2d3e
     packages=setuptools.find_packages(),
     packages_dir={"": "src"},
     classifiers=[
